48f31b850fd17b991a5fb5e1f0c757d2cc9a8055
Luanrc <luan.rodrigues.c@gmail.com>
2016-04-19 21:06:36 -0300

    primeiro exemplo pixi.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var http = require("http");
 var express = require("express");
+var fs = require ('fs')
 
 var app = express();
-// app.use("/home", express.static(__dirname + "public"));
-
-app.get("/", function(req, res) {
-	res.send("allahu ahckbar");
-});
+app.use(express.static('./public'));
 
 http.createServer(app).listen(3000, function() {
 	console.log("server online na porta: ");
